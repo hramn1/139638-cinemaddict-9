d203c8ce350d211935b7cd46e969584ffe0179da
hramn1 <stmihailov@yandex.ru>
2019-08-13 22:30:29 +0300

    module2-task1-past4

--- src/components/most-comment.js
+++
@@ -1,6 +0,0 @@
-export const addMostComment = () =>
-  `<section class="films-list--extra">
-      <h2 class="films-list__title">Most commented</h2>
-      <div class="films-list__container">
-      </div>
-    </section>`;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {addShowMore} from './components/button.js';
 import {addfilmCard} from './components/film-card.js';
 import {addMenu} from './components/menu.js';
-import {addMostComment} from './components/most-comment.js';
 import {addPopup} from './components/popup.js';
 import {addTitleUser} from './components/title-user.js';
 import {addTopRated} from './components/top-rated.js';
@@ -13,21 +12,31 @@ const mainContainer = document.querySelector(`.main`);
 function render(container, layout) {
   container.insertAdjacentHTML(`beforeend`, layout);
 }
-render (headerContainer, addSearch());
-render (headerContainer, addTitleUser());
-render (mainContainer, addMenu());
-render (mainContainer, addFilmContainer());
+render(headerContainer, addSearch());
+render(headerContainer, addTitleUser());
+render(mainContainer, addMenu());
+render(mainContainer, addFilmContainer());
 const filmContainer = document.querySelector(`.films`);
 const filmList = filmContainer.querySelector(`.films-list`);
-const filmCardContainer = filmList.querySelector(`.films-list__container`)
-render (filmList, addShowMore());
-for (let i = 0; i < 5; i++){
-  render(filmCardContainer,addfilmCard());
+const filmCardContainer = filmList.querySelector(`.films-list__container`);
+render(filmList, addShowMore());
+for (let i = 0; i < 5; i++) {
+  render(filmCardContainer, addfilmCard());
 }
-for (let j = 0; j < 2; j++){
-  render (filmContainer, addTopRated());
+for (let j = 0; j < 2; j++) {
+  render(filmContainer, addTopRated());
 }
+const filmExtraTitle = document.querySelectorAll(`.films-list--extra .films-list__title`);
+filmExtraTitle.forEach(function (item, i) {
+  if (i === 0) {
+    item.textContent = `Top rated`;
+  } else {
+    item.textContent = `Most comment`;
+  }
+});
 const filmExtraContainer = document.querySelectorAll(`.films-list--extra .films-list__container`);
-for (let k = 0; k < 2; k++){
-  render(filmExtraContainer[k],addfilmCard());
-}
+filmExtraContainer.forEach(function () {
+  for (let k = 0; k < 2; k++) {
+    render(filmExtraContainer[k], addfilmCard());
+  }
+});
