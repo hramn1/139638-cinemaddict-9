7d72ae80569abb37e4aeddd2cc8342bdb3310da5
hramn1 <stmihailov@yandex.ru>
2019-08-28 19:46:19 +0300

    module4-task1

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,6 +1,29 @@
-export const addSort = () =>
-  `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`;
+  import {createElement} from "../utils.js";
+  class Sort {
+    constructor() {
+      this._element = null;
+    }
+
+    getElement() {
+      if (!this._element) {
+        this._element = createElement(this.getTemplate());
+      }
+
+      return this._element;
+    }
+
+    removeElement() {
+      remove(this._element);
+      this._element = null;
+    }
+
+
+    getTemplate() {
+      return   `<ul class="sort">
+          <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+          <li><a href="#" class="sort__button">Sort by date</a></li>
+          <li><a href="#" class="sort__button">Sort by rating</a></li>
+        </ul>`;
+    }
+  }
+  export {Sort}
--- src/components/title-user.js
+++ src/components/title-user.js
@@ -1,5 +1,30 @@
-export const addTitleUser = (generateRank) =>
-  `<section class="header__profile profile">
-    <p class="profile__rating">${generateRank}</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>`;
+  import {createElement} from "../utils.js";
+  class TitleUser {
+    constructor(Title) {
+      this._element = null;
+      this._titleUser = Title.generateRank;
+
+    }
+
+    getElement() {
+      if (!this._element) {
+        this._element = createElement(this.getTemplate());
+      }
+
+      return this._element;
+    }
+
+    removeElement() {
+      remove(this._element);
+      this._element = null;
+    }
+
+
+    getTemplate() {
+      return `<section class="header__profile profile">
+          <p class="profile__rating">${_titleUser}</p>
+          <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+        </section>`;
+    }
+  }
+  export {TitleUser}
